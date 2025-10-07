db0b11ff338369724a412ac20291aa9efe90ad45
pc <18928974030@163.com>
2025-10-08 00:36:28 +0800

    fix: update site URL and base path for Vercel

--- astro.config.js
+++ astro.config.js
@@ -7,11 +7,11 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   // 请修改为你自己的线上地址，谢谢茄子
-  site: 'https://tcdw.github.io/koi',
+  site: 'https://pcblog-ashy.vercel.app',
 
   // 如果你的网站在子路径下（例如 `https://example.com/koi/`），则填写 `/koi/`
   // 在根路径下（例如 `https://example.com/`）则填写 `/`
-  base: process.env.NODE_ENV === "production" ? "/koi/" : "",
+  //base: process.env.NODE_ENV === "production" ? "/koi/" : "",
 
   integrations: [mdx(), sitemap(), svelte()],
 
